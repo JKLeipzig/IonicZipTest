cf9114b5ce73b126077472fc0716beb5262b7b02
JKLeipzig <34030208+JKLeipzig@users.noreply.github.com>
2017-11-27 14:13:12 +0100

    Add files via upload

---
+++ IonicZipTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Ionic.Zip;
+
+namespace IonicZipTest
+{
+   class Program
+   {
+      static void doZipFile(String fileName, String zipName, String password)
+      {
+         using (ZipFile zip = new ZipFile())
+         {
+            zip.UseZip64WhenSaving = Zip64Option.AsNecessary;
+            zip.Password = password;
+            zip.Encryption = EncryptionAlgorithm.WinZipAes256;
+            zip.AddFile(fileName, "");
+            zip.Save(zipName);
+         }
+      }
+
+      static void Main(string[] args)
+      {
+         String fileEmpty = "../../TestFileEmpty.txt";
+         String zipEmpty = "../../TestFileEmpty.zip";
+
+         String fileFilled = "../../TestFileFilled.txt";
+         String zipFilled = "../../TestFileFilled.zip";
+
+         String password = "123456";
+
+         // this one creates zip withour encryption
+         doZipFile(fileEmpty, zipEmpty, password);
+
+         // this one works as excpeted
+         doZipFile(fileFilled, zipFilled, password);
+      }
+   }
+}
